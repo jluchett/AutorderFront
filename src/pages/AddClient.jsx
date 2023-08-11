96f821898fc39b91818fd55088620ce3434dc311
jluchett <jluchett@hotmail.com>
2023-08-10 23:38:43 -0500

    Fixed some bugs on AddClient page

--- src/pages/AddClient.jsx
+++ src/pages/AddClient.jsx
@@ -1,66 +1,114 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import HeaderBar from '../components/HeaderBar';
-import { Link } from 'react-router-dom';
-import Footer from '../components/Footer';
+import React, { useState } from "react";
+import HeaderBar from "../components/HeaderBar";
+import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
+
+const initialForm = {
+  id: "",
+  nombre: "",
+  telefono: "",
+  email: "",
+};
 
 const AddClient = () => {
+  const [client, setClient] = useState(initialForm);
+  const [errorMesage, setErrorMesage] = useState("");
+  const [succesMesage, setSuccesMesage] = useState("");
+
+  const handleChange = (e) => {
+    setClient(...client, (e.target.name = e.target.value));
+  };
+
+  const handledSubmit = () =>{
+    console.log("Hola enviaste el form")
+    setErrorMesage("")
+    setSuccesMesage("")
+  }
   return (
     <>
-      <header>
-        <HeaderBar />
-      </header>
-      <main>
-        <section className="info-user">
-          <div className="encabezado">
-            <div className="user-add">
-              <i className="fa-solid fa-user-plus"></i>
+      <HeaderBar />
+      <div className="info-user">
+        <div className="encabezado">
+          <div className="user-add">
+            <i className="fa-solid fa-user-plus"></i>
+          </div>
+          <h2>Agregar usuario</h2>
+        </div>
+        <div className="form-add">
+          <form>
+            <div className="input-container">
+              <input
+                type="text"
+                placeholder=" "
+                required
+                className="input-float"
+                name="id"
+                value={client.id}
+                onChange={handleChange}
+              />
+              <label className="label-float">Número identificación</label>
             </div>
-            <h2>Agregar Cliente</h2>
-          </div>
-          <div className="form-add">
-            <form>
-              <div className="input-container">
-                <label htmlFor="id" className="label-float">
-                  Número identificación
-                </label>
-                <input
-                  type="text"
-                  placeholder=" "
-                  required
-                  className="input-float"
-                  id="id"
-                  value={""}
-                  onChange={(e) => {
-                   console.log(e)
-                  }}
-                />
-              </div>
-              
-              <div>
-                <button className="add-button" onClick={"handledSubmit"}>
-                  Guardar
-                </button>
-              </div>
-            </form>
-          </div>
-          <nav className="enlaces">
-            <Link className="regresar-button" to={"/"}>
-              Regresar a Home
-            </Link>
-            <span className="separador"></span>
-            <Link className="regresar-button" to={"/clients"}>
-              Regresar a clientes
-            </Link>
-          </nav>
-        </section>
-      </main>
-      <footer>
-        <Footer />
-      </footer>
+            <div className="input-container">
+              <input
+                type="text"
+                placeholder=" "
+                required
+                className="input-float"
+                name="nombre"
+                value={client.nombre}
+                onChange={handleChange}
+              />
+              <label className="label-float">Nombre cliente</label>
+            </div>
+            <div className="input-container">
+              <input
+                type="text"
+                placeholder=" "
+                required
+                className="input-float"
+                name="telefono"
+                value={client.telefono}
+                onChange={handleChange}
+              />
+              <label className="label-float">Numero celular</label>
+            </div>
+            <div className="input-container">
+              <input
+                type="text"
+                placeholder=" "
+                required
+                className="input-float"
+                name="email"
+                value={client.email}
+                onChange={handleChange}
+              />
+              <label className="label-float">Email cliente</label>
+            </div>
+            {errorMesage && (
+              <span className="error-message">{errorMesage}</span>
+            )}
+            {succesMesage && (
+              <span className="success-message">{succesMesage}</span>
+            )}
+            <button className="add-button" onClick={handledSubmit}>
+              Guardar
+            </button>
+          </form>
+        </div>
+        <div className="enlaces">
+          <Link className="regresar-button" to={"/"}>
+            Regresar a Home
+          </Link>
+          <span className="separador"></span>
+          <Link className="regresar-button" to={"/clients"}>
+            Regresar a Clientes
+          </Link>
+        </div>
+      </div>
+      <Footer />
     </>
-  
-  )
-}
+  );
+};
 
-export default AddClient
+export default AddClient;
